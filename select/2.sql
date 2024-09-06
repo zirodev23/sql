b1cef503287d67b7d6bdd562e9dfcafcf13a8f37
Renars <your@email.example>
2024-09-06 09:20:42 +0300

    select for certain fields, custom fields; + distinct

---
+++ select/2.sql
@@ -0,0 +1,13 @@
+-- uzdevums: atlasīt datus no tabulas 'customers'
+-- datubāzē 'sql_store'
+-- atlasīt vārdu, uzvārdu un punktus
+
+use sql_store;
+-- select
+-- 	first_name,
+--     last_name,
+--     points,
+--     points+100 as "points increased"
+-- from customers;
+
+select distinct state from customers;
