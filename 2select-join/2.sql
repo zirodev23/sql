8fbfaef9ea3200bd11a557b9f5be7793d8b29383
Renars <your@email.example>
2024-09-12 09:21:05 +0300

    join orders and customers; ON vs USING

---
+++ 2select-join/2.sql
@@ -0,0 +1,35 @@
+-- 'sql_store' datubāze.
+-- Atlasi datus no 'orders' un 'customers' tabulām,
+-- lai var redzēt klienta vārdu pie katra
+-- pasūtījuma ieraksta
+
+-- izmantojam alias
+
+SELECT 
+    o.order_id,
+    c.customer_id,
+    c.first_name,
+    c.last_name
+FROM orders o
+JOIN customers c
+	ON o.customer_id = c.customer_id;
+
+-- iegūt to pašu rezultātu ar USING
+SELECT 
+    o.order_id,
+    c.customer_id,
+    c.first_name,
+    c.last_name
+FROM orders o
+JOIN customers c
+	USING (customer_id);
+    
+-- kreisajā pusē paņemt customers
+SELECT 
+    o.order_id,
+    c.customer_id,
+    c.first_name,
+    c.last_name
+FROM customers c
+JOIN orders o
+	ON o.customer_id = c.customer_id;
